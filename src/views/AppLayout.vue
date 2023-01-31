f23dc24ddbaca08d60638d2e166b461b419ee14a
A. Osama <Altimos@desktop.com>
2023-02-01 00:51:36 +0200

    Minor style modification

--- src/views/AppLayout.vue
+++ src/views/AppLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-6">
         <router-view />
-        <hr />
+        <hr class="mt-3"/>
         <div class="flex justify-center items-center mt-3">
             <h1 class="">Scandiweb Test assignment</h1>
         </div>
